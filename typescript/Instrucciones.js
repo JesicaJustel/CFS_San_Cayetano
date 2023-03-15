d02a7a85b018c01e74f2cd51a87398fef970ed26
JesicaJustel <justeljesica@gmail.com>
2023-03-15 15:27:13 -0300

    holaMundo.ts

---
+++ typescript/Instrucciones.js
@@ -0,0 +1,5 @@
+console.log("instrucciones para hacer una torta.ts");
+console.log("1 paso:en un bol, poner harina");
+console.log("2 paso:agregar huevos con azucar");
+console.log("mezclar todo con leche");
+console.log("pasar la preparacion al molde y hornear");
